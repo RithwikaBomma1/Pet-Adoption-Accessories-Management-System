26865ef4f57cf648e0d816a1342403b491548123
Rithwika5 <139744048+Rithwika5@users.noreply.github.com>
2024-04-27 14:19:49 +0530

    Pet-Adoption-Sysytem

---
+++ food-del/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Pets Adoption And Accessories</h2>
+                <p>Care. Adopt. love.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
